daab83c74e8a7543d2e1b5c64bf68956b0b1100f
neh-panchal <nehpanchal10@gmail.com>
2025-01-23 18:33:50 +0530

    Initial Commit

---
+++ nectar_app/nectar_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nectar_appApp.swift
+//  nectar_app
+//
+//  Created by student on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct nectar_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
