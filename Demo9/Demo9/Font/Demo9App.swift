0b35f0ef541e690c77dad46211eec8556a7e9cc8
aksh-1h <narwaniaksh57@gmail.com>
2025-01-24 18:09:41 +0530

    changes added

---
+++ Demo9/Demo9/Font/Demo9App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo9App.swift
+//  Demo9
+//
+//  Created by student on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Demo9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
